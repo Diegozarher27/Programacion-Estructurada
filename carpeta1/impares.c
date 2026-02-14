3d05a4bf05263f00173aa681e1b49d874007daf7
Diegozarher27 <diego.zaragoza9239@alumnos.udg.mx>
2026-02-14 12:18:51 -0600

    Add files via upload
    programas hechos en clase de programacion

---
+++ carpeta1/impares.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Escribe un numero\n");
+    int num;
+    scanf("%d",&num);
+    if (num%2 == 1){
+        printf("tu numero es impar\n");
+    }
+    else {
+        printf("tu numero no es impar");
+    }
+
+}
+
+
+
