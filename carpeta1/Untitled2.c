86c00b50a191e229d7bfd9ae23dcaac13e3ceaf2
Diegozarher27 <diego.zaragoza9239@alumnos.udg.mx>
2026-02-20 08:57:13 -0600

    Add files via upload

---
+++ carpeta1/Untitled2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int suma = 0;
+    scanf("%d",&x);
+
+    while (x != 0){
+        suma += x;
+        printf("%d\n", suma);
+        scanf("%d", &x);
+    }
+}if (calificacion > 100){
+            printf("eh we no se puede poner numeros mayores a 100\n");
+        }
